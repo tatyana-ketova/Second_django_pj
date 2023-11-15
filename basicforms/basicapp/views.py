db8370093653c6c5dc8bc7d242fd7df44daeebc1
tatyana-ketova <tatyana.i.ketova@gmail.com>
2023-11-15 16:56:37 +0100

    create form for users and save data from users to db

---
+++ basicforms/basicapp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from basicforms import forms
+from basicapp import models
+# Create your views here.
+
+
+def index(request):
+    return render(request,'basicapp/index.html')
+
+
+def form_name_view(request):
+    form = forms.FormName()
+    if request.method == "POST":
+        form = forms.FormName(request.POST)
+        if form.is_valid():
+            instance = models.UserModel(
+                name=form.cleaned_data['name'],
+                email=form.cleaned_data['email'],
+                text=form.cleaned_data['text']
+            )
+            instance.save()
+            print("Success")
+            print('Name:'+form.cleaned_data['name'])
+            print('Email:' + form.cleaned_data['email'])
+            print('Text:' + form.cleaned_data['text'])
+
+
+
+    return render(request,'basicapp/form.html',{'form': form})
